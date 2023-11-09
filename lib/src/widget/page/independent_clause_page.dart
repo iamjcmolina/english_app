805c047eb21277454348225c9375c1149f7b62bb
Carlos Molina <iamjcmolina@gmail.com>
2023-11-08 20:04:54 -0600

    Move ClauseText to private classes

--- lib/src/widget/page/independent_clause_page.dart
+++ lib/src/widget/page/independent_clause_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../extensions/string_extension.dart';
 import '../../model/label.dart';
 import '../../model/nullable.dart';
 import '../../model/sentence/adverb/adverb_position.dart';
@@ -10,11 +11,11 @@ import '../../model/sentence/clause/tense.dart';
 import '../../model/sentence/noun/any_noun.dart';
 import '../../model/sentence/noun/subject_complement.dart';
 import '../../model/sentence/verb/any_verb.dart';
+import '../../model/sentence/verb/phrasal_verb.dart';
 import '../../model/word.dart';
 import '../common/item_editor_layout.dart';
 import '../common/item_tile.dart';
 import '../common/sentence_scaffold.dart';
-import '../sentence/clause/clause_text.dart';
 import '../sentence/verb/first_auxiliary_verb_tile.dart';
 import '../sentence/verb/verb_tile.dart';
 import 'adverbial_phrase_page.dart';
@@ -105,7 +106,10 @@ class _IndependentClausePageState extends State<IndependentClausePage> {
             ],
           ),
           ListTile(
-            title: ClauseText(clause: clause),
+            title: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [_ClauseTextEn(clause), _ClauseTextEs(clause)],
+            ),
           ),
         ],
         body: [
@@ -310,4 +314,122 @@ class _IndependentClausePageState extends State<IndependentClausePage> {
       setState(() => isMainVerbFieldShown = !isMainVerbFieldShown);
 
   toggleIsFormShown() => setState(() => isTenseFieldShown = !isTenseFieldShown);
+}
+
+class _ClauseTextEn extends StatelessWidget {
+  final IndependentClause clause;
+
+  const _ClauseTextEn(this.clause);
+
+  @override
+  Widget build(BuildContext context) {
+    final aux = clause.auxiliaryVerbs;
+
+    final firstAuxiliaryVerbSpan = TextSpan(
+        text: aux.first?.addSpace(),
+        style: aux.first == null ? Word.empty.style : Word.verb.style);
+
+    return Text.rich(TextSpan(
+      children: [
+        TextSpan(
+            text: !clause.isInterrogative
+                ? clause.frontAdverb?.en.addSpace()
+                : null,
+            style: Word.adverb.style),
+        if (clause.isInterrogative) firstAuxiliaryVerbSpan,
+        TextSpan(
+            text: (clause.subject?.en ?? Label.subject)
+                .addSpace(!clause.isVerbContractionActive),
+            style: clause.subject == null ? Word.empty.style : Word.noun.style),
+        if (!clause.isInterrogative) firstAuxiliaryVerbSpan,
+        TextSpan(
+            text: clause.midAdverb?.en.addSpace(), style: Word.adverb.style),
+        TextSpan(text: aux.second?.addSpace(), style: Word.verb.style),
+        TextSpan(text: aux.third?.addSpace(), style: Word.verb.style),
+        TextSpan(
+            text: clause.isBeAuxiliary
+                ? null
+                : (clause.conjugateVerb() ?? clause.verbPlaceholder).addSpace(),
+            style: clause.verb == null ? Word.empty.style : Word.verb.style),
+        TextSpan(
+            text: clause.hasDitransitiveVerb
+                ? clause.indirectObject?.en.addSpace()
+                : null,
+            style: Word.noun.style),
+        TextSpan(
+            text: clause.hasTransitiveVerb
+                ? clause.directObject?.en.addSpace()
+                : null,
+            style: Word.noun.style),
+        TextSpan(
+            text: clause.verb is PhrasalVerb
+                ? (clause.verb as PhrasalVerb).particle.addSpace()
+                : null,
+            style: Word.verb.style),
+        TextSpan(
+            text: clause.canHaveSubjectComplement
+                ? clause.subjectComplement?.en.addSpace()
+                : null,
+            style: Word.noun.style),
+        TextSpan(text: clause.endAdverb?.en, style: Word.adverb.style),
+        TextSpan(text: clause.isInterrogative ? '?' : null),
+      ],
+    ));
+  }
+}
+
+class _ClauseTextEs extends StatelessWidget {
+  final IndependentClause clause;
+
+  const _ClauseTextEs(this.clause);
+
+  @override
+  Widget build(BuildContext context) {
+    final aux = clause.auxiliaryVerbs;
+
+    return Text.rich(TextSpan(
+      children: [
+        TextSpan(
+            text: !clause.isInterrogative
+                ? clause.frontAdverb?.es.addSpace()
+                : null,
+            style: Word.adverb.style),
+        TextSpan(
+            text: (clause.subject?.es ?? Label.subjectEs).addSpace(),
+            style: clause.subject == null ? Word.empty.style : Word.noun.style),
+        TextSpan(
+            text: clause.midAdverb?.es.addSpace(), style: Word.adverb.style),
+        TextSpan(
+            text: clause.hasDitransitiveVerb
+                ? clause.indirectObject?.es.addSpace()
+                : null,
+            style: Word.noun.style),
+        TextSpan(
+            text: clause.hasTransitiveVerb
+                ? clause.directObject?.es.addSpace()
+                : null,
+            style: Word.noun.style),
+        TextSpan(
+            text: aux.firstEs?.addSpace(aux.firstEs!.isNotEmpty),
+            style: aux.firstEs == null ? Word.empty.style : Word.verb.style),
+        TextSpan(text: aux.secondEs?.addSpace(), style: Word.verb.style),
+        TextSpan(text: aux.thirdEs?.addSpace(), style: Word.verb.style),
+        TextSpan(
+            text: clause.isBeAuxiliary
+                ? null
+                : (clause.conjugateVerbEs() ?? clause.verbPlaceholderEs)
+                    .addSpace(),
+            style: clause.verb == null ? Word.empty.style : Word.verb.style),
+        TextSpan(
+            text: clause.canHaveSubjectComplement
+                ? clause.subjectComplement
+                    ?.toEs(clause.hasPluralSubject)
+                    .addSpace()
+                : null,
+            style: Word.noun.style),
+        TextSpan(text: clause.endAdverb?.es, style: Word.adverb.style),
+        TextSpan(text: clause.isInterrogative ? '?' : null),
+      ],
+    ));
+  }
 }
